c1f9323850d20d2c4d37af57c971f68f03a5e7c9
Jordan Liu <32057465+Jordan-Liu1994@users.noreply.github.com>
2022-08-15 13:08:20 +0800

    Add files via upload

---
+++ Test_Java_001.java
@@ -0,0 +1,14 @@
+package test_java;
+
+public class Test_Java_001 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi QC");
+		
+		System.out.println("-----Hello World!-----");
+		System.out.println("-----Hello Simplilearn!-----");
+		System.out.println("-----Hello QA/QC Team!-----");
+	}
+
+}
